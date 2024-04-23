b8818278435b1e42c7bfd47d7e4c01ac7411b267
Hani Husamuddin <hani.husam@gmail.com>
2024-04-23 20:13:59 +0700

    refactor: user remeber lib

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,6 +1,5 @@
 import {PrismaClient} from '@prisma/client'
 
-import {singleton} from './singleton.server'
+import {remember} from '@epic-web/remember'
 
-// Hard-code a unique key, so we can look up the client when this module gets re-imported
-export const db = singleton('prisma', () => new PrismaClient())
+export const db = remember('prisma', () => new PrismaClient())
